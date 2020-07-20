8f3f1b419fe0fc39020ff3c1008391d6a1503c56
Michael Nguyen <michaelnguyenz229@gmail.com>
2020-07-20 15:47:18 -0700

    read mux

--- csr.v
+++ csr.v
@@ -16,11 +16,15 @@ module csr
 );
 
 //internal signals
-csr_reg_hit [2:0];
+reg csr_reg_hit [2:0];
 
-csr_control_reg [31:0];
-csr_status_reg [31:0];
-csr_descriptor_pointer_reg [31:0];
+reg csr_wr_en_reg [2:0];
+
+reg csr_control_reg [31:0];
+reg csr_status_reg [31:0];
+reg csr_descriptor_pointer_reg [31:0];
+
+reg rd_data_mux [31:0];
 
 //state machine
 typedef enum logic [2:0] {IDLE, WR_EN, WAIT_READ_1, WAIT_READ_2, RD_VALID} State;
@@ -60,6 +64,8 @@ always @*
             next_state <= IDLE;
     endcase
 
+    //state machine output assignement
+
     //decoder
     always @*
     begin
@@ -83,33 +89,91 @@ always @*
     always @ (posedge clk)
         if(reset)
             csr_control_reg[7:0] <= 8'h0; 
-        else if(csr_wr_en_reg[0] & csr_be_i[0])
+        else if(csr_wr_en_reg[0] & csr_be_i[0] & wr_en_state)
             csr_control_reg[7:0] <= csr_wr_data_i[7:0];
 
     always @ (posedge clk)
         if(reset)
             csr_control_reg[15:8] <= 8'h0; 
-        else if(csr_wr_en_reg[0] & csr_be_i[1])
+        else if(csr_wr_en_reg[0] & csr_be_i[1] & wr_en_state)
             csr_control_reg[15:8] <= csr_wr_data_i[15:8];
 
     always @ (posedge clk)
         if(reset)
             csr_control_reg[23:16] <= 8'h0; 
-        else if(csr_wr_en_reg[0] & csr_be_i[2])
+        else if(csr_wr_en_reg[0] & csr_be_i[2] & wr_en_state)
             csr_control_reg[23:16] <= csr_wr_data_i[23:16];
 
     always @ (posedge clk)
         if(reset)
             csr_control_reg[31:24] <= 8'h0; 
-        else if(csr_wr_en_reg[0] & csr_be_i[3])
+        else if(csr_wr_en_reg[0] & csr_be_i[3] & wr_en_state)
             csr_control_reg[31:24] <= csr_wr_data_i[31:24];
     
     //Status Register
+    always @ (posedge clk)
+        if(reset)
+            csr_status_reg[7:0] <= 8'h0; 
+        else if(csr_wr_en_reg[1] & csr_be_i[0] & wr_en_state)
+            csr_status_reg[7:0] <= csr_wr_data_i[7:0];
+
+    always @ (posedge clk)
+        if(reset)
+            csr_status_reg[15:8] <= 8'h0; 
+        else if(csr_wr_en_reg[1] & csr_be_i[1] & wr_en_state)
+            csr_status_reg[15:8] <= csr_wr_data_i[15:8];
+
+    always @ (posedge clk)
+        if(reset)
+            csr_status_reg[23:16] <= 8'h0; 
+        else if(csr_wr_en_reg[1] & csr_be_i[2] & wr_en_state)
+            csr_status_reg[23:16] <= csr_wr_data_i[23:16];
+
+    always @ (posedge clk)
+        if(reset)
+            csr_status_reg[31:24] <= 8'h0; 
+        else if(csr_wr_en_reg[1] & csr_be_i[3] & wr_en_state)
+            csr_status_reg[31:24] <= csr_wr_data_i[31:24];
 
     //Next Descriptor Pointer Register
+    always @ (posedge clk)
+        if(reset)
+            csr_descriptor_pointer_reg[7:0] <= 8'h0; 
+        else if(csr_wr_en_reg[2] & csr_be_i[0] & wr_en_state)
+            csr_descriptor_pointer_reg[7:0] <= csr_wr_data_i[7:0];
 
+    always @ (posedge clk)
+        if(reset)
+            csr_descriptor_pointer_reg[15:8] <= 8'h0; 
+        else if(csr_wr_en_reg[2] & csr_be_i[1] & wr_en_state)
+            csr_descriptor_pointer_reg[15:8] <= csr_wr_data_i[15:8];
 
+    always @ (posedge clk)
+        if(reset)
+            csr_descriptor_pointer_reg[23:16] <= 8'h0; 
+        else if(csr_wr_en_reg[2] & csr_be_i[2] & wr_en_state)
+            csr_descriptor_pointer_reg[23:16] <= csr_wr_data_i[23:16];
 
+    always @ (posedge clk)
+        if(reset)
+            csr_descriptor_pointer_reg[31:24] <= 8'h0; 
+        else if(csr_wr_en_reg[2] & csr_be_i[3] & wr_en_state)
+            csr_descriptor_pointer_reg[31:24] <= csr_wr_data_i[31:24];
+
+    //csr read mux
+    always @* 
+        begin
+            case(csr_wr_en_reg[2:0])
+                3'b001: rd_data_mux[31:0] <= csr_control_reg[31:0];
+                3'b010: rd_data_mux[31:0] <= csr_status_reg[31:0];
+                3'b100: rd_data_mux[31:0] <= csr_descriptor_pointer_reg[31:0];
+                default: rd_data_mux[31:0] <= 32'h0;
+        end
+    always @ (posedge clk)
+        if(reset)
+            csr_rd_data_o[31:0] <= 32'h0;
+        else
+            csr_rd_data_o[31:0] <= rd_data_mux[31:0];
 
 endmodule
 
