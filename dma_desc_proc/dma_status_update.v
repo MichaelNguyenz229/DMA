b589cd32063080e38b9c178087fff5bf6c6cd709
Michael Nguyen <michaelnguyenz229@gmail.com>
2020-07-30 16:34:51 -0700

    compile file

--- dma_desc_proc/dma_status_update.v
+++ dma_desc_proc/dma_status_update.v
@@ -4,22 +4,28 @@ module dma_status_update
     input reset,
 
     //status from write block
+    input dma_status_fifo_wr_req_i,
     input [24:0]dma_wr_done_status_i,
+
+    //status to write block
+    output dma_status_fifo_almost_full_o,
 
     //from csr
     input [31:0] csr_control_i,
+    input [31:0] csr_status_i,
+    input [31:0] csr_first_pointer_i,
 
     //to update csr status
-    output [31:0] csr_status_update_o,
-    output [3:0] csr_status_update_be_o,
-    output csr_status_update_rq_o,
+    output [31:0] csr_status_update_data_o,
+    output csr_status_update_req_o,
     input csr_status_update_ack_i,
 
     //to AVMM master (update descriptor)
     output dma_desc_update_wr_o,
     output [31:0] dma_desc_update_data_o,
     output [3:0] dma_desc_update_be_o,
-    input dma_desc_update_wait_rq,
+    output reg [31:0] dma_desc_update_addr_o,
+    input dma_desc_update_wait_req_i,
 
     //interupt output
     output dma_interupt_rq_o,
@@ -38,19 +44,31 @@ wire irq_state;
 
 wire fifo_empty;
 
+reg [24:0] status_fifo_data_q;
+reg [24:0] status_fifo_data_reg;
+
+wire ie_desc;
+wire ie_chain;
+
+wire last_desc;
+
+wire status_clear;
+
+wire [31:0] dma_desc_update_addr;
+
 //status fifo
 scfifo	status_fifo (
 				.clock (clk),
 				.data (dma_wr_done_status_i),
 				.rdreq (rd_fifo_state),
 				.sclr (reset),
-				.wrreq (dma_desc_fifo_wr_i),
-				.almost_full (dma_desc_fifo_almost_full),
-				.q (dma_desc_fifo_out),
+				.wrreq (dma_status_fifo_wr_req_i),
+				.almost_full (dma_status_fifo_almost_full_o),
+				.q (status_fifo_data_q),
 				.aclr (),
-				.almost_empty (fifo_empty),
+				.almost_empty (),
 				.eccstatus (),
-				.empty (),
+				.empty (fifo_empty),
 				.full (),
 				.usedw ());
 	defparam
@@ -84,20 +102,41 @@ always @(posedge clk)
 
 always @*
     case(current_state)
-    IDLE:
-        if(~fifo_empty)
 
-    RD_FIFO:
+        IDLE:
+            if(~fifo_empty)
+                next_state <= RD_FIFO;
+            else
+                next_state <= IDLE;
 
-    LD_REG:
+        RD_FIFO:
+            next_state <= LD_REG;
 
-    UPDATE_CSR_STATUS:
+        LD_REG:
+            next_state <= UPDATE_CSR_STATUS;
 
-    UPDATE_DESC:
+        UPDATE_CSR_STATUS:
+            if(csr_status_update_ack_i)
+                next_state <= UPDATE_DESC;
+            else
+                next_state <= UPDATE_CSR_STATUS;
 
-    IRQ:
+        UPDATE_DESC:
+            if(~dma_desc_update_wait_rq & ~ie_chain & ~ie_desc)
+                next_state <= IDLE;
+            else if(~dma_desc_update_wait_rq & (ie_chain | ie_desc))
+                next_state <= IRQ;
+            else
+                next_state <= UPDATE_DESC;
+                
+        IRQ:
+            if(status_clear)
+                next_state <= IDLE;
+            else
+                next_state <= IRQ;
 
-    default:
+        default:
+            next_state <= IDLE;
 
     endcase
 
@@ -108,5 +147,38 @@ assign ld_reg_state = (current_state[2:0] == LD_REG);
 assign update_csr_state_state = (current_state[2:0] == UPDATE_CSR_STATUS);
 assign update_desc_state = (current_state[2:0] == UPDATE_DESC);
 assign irq_state = (current_state[2:0] == IRQ);
+
+//IE assignment
+assign last_desc = status_fifo_data_reg[24];
+assign ie_desc = (csr_control_i[2] | csr_control_i[4]);
+assign ie_chain = ((csr_control_i[3] & last_desc) | (csr_control_i[4] & last_desc));
+
+//Status clear assignment
+assign status_clear = ~csr_status_i[2] & ~csr_status_i[3];
+
+//Latch status register
+always @ (posedge clk)
+    if(ld_reg_state)
+        status_fifo_data_reg[24:0] <= status_fifo_data_q[24:0];
+
+//csr status update interface
+assign csr_status_update_req_o = update_csr_state;
+assign csr_status_update_data_o[31:0] = {csr_status_i[31:4], status_fifo_data_reg[24], 1'b1, csr_status_i[1:0]};
+
+//master write interface for descriptor update
+assign dma_desc_update_wr_o = update_desc_state;
+assign dma_desc_update_data_o[31:0] = {16'h0, status_fifo_data_reg[15:0]};
+assign dma_desc_update_be_o[3:0] = 4'b1100;
+assign dma_desc_update_addr[31:0] = csr_first_pointer_i + (status_fifo_data_reg[23:16] * 5'h1c);
+
+//desc update addr register
+always @ posedge(clk)
+    if(reset)
+        dma_desc_update_addr_o[31:0] <= 32'h0;
+    else
+        dma_desc_update_addr_o[31:0] <= dma_desc_update_addr[31:0];
+
+//assign interupt 
+assign dma_interupt_rq_o = irq_state;
 
 endmodule
