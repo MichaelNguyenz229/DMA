c2227e602f6b1bc6bb7dad846040774a8445b350
Michael Nguyen <michaelnguyenz229@gmail.com>
2020-07-31 16:37:20 -0700

    compiled read block

--- dma_read_block/dma_read_block.v
+++ dma_read_block/dma_read_block.v
@@ -4,13 +4,13 @@ module dma_read_block
     input reset,
 
     //to AVMM master port
-    output [31:0] dma_source_addr_o;
-    output [10:0] dma_data_bcount_o;
+    output [31:0] rd_master_addr_o,
+    output [10:0] rd_master_bcount_o,
 
     //from AVMM master port
-    input dma_source_wait_req_i;
-    input dma_source_read_data_valid_i;
-    input [255:0] dma_source_data_i;
+    input rd_master_wait_req_i,
+    input rd_master_data_valid_i,
+    input [255:0] rd_master_data_i,
 
     //from desc processor
     input dma_rd_fifo_command_req_i,
@@ -45,8 +45,8 @@ wire [15:0] bytes_to_transfer;
 
 reg [10:0] bcount_reg;
 
-reg[255:0] dma_rd_data_reg;
-reg dma_rd_data_valid_reg;
+reg[255:0] rd_master_data_reg;
+reg rd_master_data_valid_reg;
 
 //read fifo data
 assign rd_fifo_data = {dma_rd_bytes_to_transfer_i, dma_rd_addr_i};
@@ -59,7 +59,7 @@ scfifo	read_block_fifo (
 				.sclr (reset),
 				.wrreq (dma_rd_fifo_command_req_i),
 				.almost_full (),
-				.q (status_fifo_data_q),
+				.q (rd_fifo_data_q),
 				.aclr (),
 				.almost_empty (),
 				.eccstatus (),
@@ -67,17 +67,17 @@ scfifo	read_block_fifo (
 				.full (dma_rd_fifo_full_o),
 				.usedw ());
 	defparam
-		status_fifo.add_ram_output_register = "OFF",
-		status_fifo.almost_full_value = 24,
-		status_fifo.intended_device_family = "Cyclone V",
-		status_fifo.lpm_numwords = 32,
-		status_fifo.lpm_showahead = "OFF",
-		status_fifo.lpm_type = "scfifo",
-		status_fifo.lpm_width = 48,
-		status_fifo.lpm_widthu = 5,
-		status_fifo.overflow_checking = "ON",
-		status_fifo.underflow_checking = "ON",
-		status_fifo.use_eab = "ON";
+		read_block_fifo.add_ram_output_register = "OFF",
+		read_block_fifo.almost_full_value = 24,
+		read_block_fifo.intended_device_family = "Cyclone V",
+		read_block_fifo.lpm_numwords = 32,
+		read_block_fifo.lpm_showahead = "OFF",
+		read_block_fifo.lpm_type = "scfifo",
+		read_block_fifo.lpm_width = 48,
+		read_block_fifo.lpm_widthu = 5,
+		read_block_fifo.overflow_checking = "ON",
+		read_block_fifo.underflow_checking = "ON",
+		read_block_fifo.use_eab = "ON";
 
 //read block state machine
 localparam IDLE = 3'b00;
@@ -103,9 +103,9 @@ always @*
             next_state <= SEND_RD;
 
         SEND_RD:
-            if(~dma_source_wait_req_i)
+            if(~rd_master_wait_req_i)
                 next_state <= IDLE;
-            else:
+            else
                 next_state <= SEND_RD;
             
         default:
@@ -113,10 +113,10 @@ always @*
     endcase
 
 //state machine assignment
-idle_state = (current_state[1:0] == IDLE);
-rd_fifo_state = (current_state[1:0] == RD_FIFO);
-ld_reg_state = (current_state[1:0] == LD_REG);
-send_rd_state = (current_state[1:0] == SEND_RD);
+assign idle_state = (current_state[1:0] == IDLE);
+assign rd_fifo_state = (current_state[1:0] == RD_FIFO);
+assign ld_reg_state = (current_state[1:0] == LD_REG);
+assign send_rd_state = (current_state[1:0] == SEND_RD);
 
 //latch reg
 always @ (posedge clk)
@@ -124,23 +124,23 @@ always @ (posedge clk)
         command_fifo_reg[47:0] <= rd_fifo_data_q;
 
 //burst counter and source addr
-assign bytes_to_transfer = command_fifo_reg[47:32] 
+assign bytes_to_transfer = command_fifo_reg[47:32];
 
 always @ (posedge clk)
     if(reset)
         bcount_reg[10:0] <= bytes_to_transfer[15:5] + | command_fifo_reg[4:0];
 
-assign dma_data_bcount_o[10:0] = bcount_reg[10:0];
-assign dma_source_addr_o[31:0] = command_fifo_reg[31:0];
+assign rd_master_bcount_o[10:0] = bcount_reg[10:0];
+assign rd_master_addr_o[31:0] = command_fifo_reg[31:0];
 
 //reg for dma data and rd data valid
 always @ (posedge clk)
-    dma_rd_data_reg[255:0] <= dma_source_data_i[255:0];
+    rd_master_data_reg[255:0] <= rd_master_data_i[255:0];
 
 always @ (posedge clk)
-    dma_rd_data_valid_reg <= dma_source_read_data_valid_i;
+    rd_master_data_valid_reg <= rd_master_data_valid_i;
 
-assign dma_rd_data_o[255:0] = dma_rd_data_reg;
-assign dma_rd_data_valid_o = dma_rd_data_valid_reg;
+assign dma_rd_data_o[255:0] = rd_master_data_reg;
+assign dma_rd_data_valid_o = rd_master_data_valid_reg;
 
 endmodule
